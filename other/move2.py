3fad4dc1ffd91ea815d2446c42a987be77ab61a1
Elkku134 <nikimalmsten@hotmail.com>
2024-10-08 12:57:48 +0300

    more changes

--- other/move2.py
+++ other/move2.py
@@ -2,7 +2,25 @@ import pyautogui
 import time
 import random
 
+time_seconds=7200
+time_seconds= int(time_seconds/10)
 
+for i in range(time_seconds):
+    x = random.randint(900, 1000)
+    y = random.randint(500, 600)
+    pyautogui.moveTo(x,y,0.5)
+    pyautogui.click(x, y)
+    print(x, y, "Seconds gone", i*10)
+    time.sleep(10)
+
+
+
+
+
+
+
+
+"""
 while True:
     x = random.randint(900, 1000)
     y = random.randint(500, 600)
@@ -10,7 +28,8 @@ while True:
     pyautogui.click(x, y)
     print(x, y)
     time.sleep(5)
+"""   
     
-
+    
 
 
