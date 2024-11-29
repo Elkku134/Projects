46861fca292919d57c7c6cb0e2e73f1b73bd3c3d
Elkku134 <nikimalmsten@hotmail.com>
2024-11-29 08:48:06 +0200

    last commit

--- other/move2.py
+++ other/move2.py
@@ -2,21 +2,21 @@ import pyautogui
 import time
 import random
 
-time_seconds=7200
+how_long = 2  #in hours
+how_long = int(how_long)
+time_seconds = how_long*60*60
+#time_seconds=7200
 time_seconds= int(time_seconds/10)
 
 for i in range(time_seconds):
-    x = random.randint(900, 1000)
-    y = random.randint(500, 600)
+    x = random.randint(900, 910)
+    y = random.randint(500, 510)
     pyautogui.moveTo(x,y,0.5)
     pyautogui.click(x, y)
-    print(x, y, "Seconds gone", i*10)
+    gone = round(i/6, 2)
+    left = round((time_seconds-gone*10)/6, 2)
+    print("X-coordinate location:", x, "Y-coordinate location:", y, "Minutes gone", gone, "Minutes left", left)
     time.sleep(10)
-
-
-
-
-
 
 
 
